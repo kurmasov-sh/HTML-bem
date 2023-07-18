ee57a79a834118c304d1fcc46f12c8ce0859951c
kurmasov <kurmasov-sh@ugraweb.ru>
2023-07-18 17:59:41 +0300

    1.3 первый квартал (доработка адаптива, генерация моб меню в DOM)

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//ПАРАЛЛАКС
+
 window.onload = function (){
     const parallax = document.querySelector('.parallax');
 
@@ -90,6 +92,141 @@ window.onload = function (){
     // Add event listeners to the arrow controls
     arrowLeft.addEventListener('click', slidePrev);
     arrowRight.addEventListener('click', slideNext);
+    
+
+    //МОБИЛЬНОЕ МЕНЮ
+    function openBurger(){
+        if(window.getComputedStyle(header__burgerWrapper).display === 'none')
+        {
+            header__burgerWrapper.style.display = 'flex';
+            console.log("4")
+        }       
+    }
+    function closeBurger(){
+        if(window.getComputedStyle(header__burgerWrapper).display === 'flex')
+        {
+            header__burgerWrapper.style.display = 'none';
+            console.log("213")
+        }       
+    }
+    const header = document.querySelector('header');
+    const burgerMenu = document.createElement('div');
+    burgerMenu.setAttribute('class', 'header__burger');
+    header.appendChild(burgerMenu);
+    burgerMenu.addEventListener('click', openBurger);
+    
+    const header__burgerWrapper = document.createElement('div');
+    header__burgerWrapper.setAttribute('class', 'header__burger-wrapper');
+    burgerMenu.appendChild(header__burgerWrapper);
+
+    const header__burgerTop = document.createElement('div');
+    header__burgerTop.setAttribute('class', 'header__burger-top');
+    header__burgerWrapper.appendChild(header__burgerTop);
+
+    const logo = document.createElement('a');
+    logo.setAttribute('class', 'logo');
+    logo.setAttribute('href', '#');
+    header__burgerTop.appendChild(logo);
+
+    const header__burgerTopBtn = document.createElement('img');
+    header__burgerTopBtn.setAttribute('class', 'header__burger-top-btn');
+    header__burgerTopBtn.setAttribute('src', 'images/Group 2.7-burger_close_btn.png');
+    header__burgerTop.appendChild(header__burgerTopBtn);
+    header__burgerTopBtn.addEventListener('click', closeBurger);
+
+
+    const burgerLogo = document.createElement('img');
+    burgerLogo.setAttribute('class', 'header__burger-logo');
+    burgerLogo.setAttribute('src', 'images/Frame.jpg');
+    logo.appendChild(burgerLogo)
+
+    const header__burgerMenu = document.createElement('div');
+    header__burgerMenu.setAttribute('class', 'header__burger-menu');
+    header__burgerWrapper.appendChild(header__burgerMenu);
+
+    const headerMenu__item1 = document.createElement('a');
+    headerMenu__item1.setAttribute('class', 'link header-menu__item');
+    headerMenu__item1.setAttribute('href', '#');
+    headerMenu__item1.textContent = 'Прямая трансляция';
+    header__burgerMenu.appendChild(headerMenu__item1);
+
+    const headerMenu__item2 = document.createElement('a');
+    headerMenu__item2.setAttribute('class', 'link header-menu__item');
+    headerMenu__item2.setAttribute('href', '#');
+    headerMenu__item2.textContent = 'Ханты-Мансийск';
+    header__burgerMenu.appendChild(headerMenu__item2);
+
+    const headerMenu__item3 = document.createElement('a');
+    headerMenu__item3.setAttribute('class', 'link header-menu__item');
+    headerMenu__item3.setAttribute('href', '#');
+    headerMenu__item3.textContent = 'Новости';
+    header__burgerMenu.appendChild(headerMenu__item3);
+
+    const headerMenu__item4 = document.createElement('a');
+    headerMenu__item4.setAttribute('class', 'link header-menu__item');
+    headerMenu__item4.setAttribute('href', '#');
+    headerMenu__item4.textContent = 'Участники';
+    header__burgerMenu.appendChild(headerMenu__item4);
+    
+    const headerMenu__item5 = document.createElement('span');
+    headerMenu__item5.setAttribute('class', 'link header-menu__item header__burger-sub-menu');
+    header__burgerMenu.appendChild(headerMenu__item5);
+
+    const headerMenu__item6 = document.createElement('a');
+    headerMenu__item6.setAttribute('class', 'link');
+    headerMenu__item6.setAttribute('href', '#');
+    headerMenu__item6.textContent = 'Основная информация';
+    headerMenu__item5.appendChild(headerMenu__item6);
+
+    const headerMenu__item7 = document.createElement('a');
+    headerMenu__item7.setAttribute('class', 'link header-menu__item');
+    headerMenu__item7.setAttribute('href', '#');
+    headerMenu__item7.textContent = 'Результаты';
+    header__burgerMenu.appendChild(headerMenu__item7);
+
+    const headerMenu__item8 = document.createElement('a');
+    headerMenu__item8.setAttribute('class', 'link header-menu__item');
+    headerMenu__item8.setAttribute('href', '#');
+    headerMenu__item8.textContent = 'Медиа';
+    header__burgerMenu.appendChild(headerMenu__item8);
+
+    const headerMenu__item9 = document.createElement('a');
+    headerMenu__item9.setAttribute('class', 'link header-menu__item');
+    headerMenu__item9.setAttribute('href', '#');
+    headerMenu__item9.textContent = 'Контакты';
+    header__burgerMenu.appendChild(headerMenu__item9);
+
+    const header__burgerBottom = document.createElement('div');
+    header__burgerBottom.setAttribute('class', 'header__burger-bottom');
+    header__burgerWrapper.appendChild(header__burgerBottom); 
+
+    const header__slowVision = document.createElement('a');
+    header__slowVision.setAttribute('class', 'header__slow-vision header__slow-vision_adaptive');
+    header__slowVision.setAttribute('href', '#');
+    header__burgerBottom.appendChild(header__slowVision);
+
+    const header__languages = document.createElement('div');
+    header__languages.setAttribute('class', 'header__languages');
+    header__burgerBottom.appendChild(header__languages);
+
+    const header__languageItem1 = document.createElement('a');
+    header__languageItem1.setAttribute('class', 'link header__language-item');
+    header__languageItem1.textContent = 'RU';
+    header__languages.appendChild(header__languageItem1);
+
+    const header__languageItem2 = document.createElement('a');
+    header__languageItem2.setAttribute('class', 'link header__language-item');
+    header__languageItem2.textContent = 'EN';
+    header__languages.appendChild(header__languageItem2);
+
+    const contacts__phone = document.createElement('p');
+    contacts__phone.setAttribute('class', 'contacts__phone');
+    contacts__phone.textContent = '+7 (3467) 555-321'
+    header__burgerBottom.appendChild(contacts__phone);
+    const contacts__address = document.createElement('p');
+    contacts__address.setAttribute('class', 'contacts__address contacts__address_burger');
+    contacts__address.textContent = 'г. Ханты-Мансийск ул. Лопарева,6'
+    header__burgerBottom.appendChild(contacts__address);
 
 }
 
