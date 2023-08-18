ccf6a2967b6dfc563928f538c338692cb76ef0c2
kurmasov <kurmasov-sh@ugraweb.ru>
2023-08-18 16:40:57 +0300

    2.1 первый квартал (добавил npm, yarn, gulp)

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,35 @@
-function defaultTask(done){
-    console.log('Task done');
+let gulp = require('gulp');
+let rename = require('gulp-rename');
+let sass = require('gulp-sass')(require('sass'));
+let imagemin = require('gulp-imagemin');
+
+//сборщик и минификатор sass
+function sassToCss(done) {
+    gulp.src('./sass/style.sass')
+        .pipe(sass({
+            errorLogToConsole: true,
+            outputStyle: 'compressed'
+        }))
+        .on('error', console.error.bind(console))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist/css/'));
+    done();
+}
+//Минификатор изображений
+function imgMin(){
+    gulp.src('./images/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('dist/images/'))
 }
 
-exports.default = defaultTask;
+function JSMin() {
+        gulp.src('./menu.js')
+        .pipe(babel())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/'))
+        .pipe(browserSync.stream());
+}
+
+//экспорт задач
+exports.default = gulp.series(sassToCss, imgMin);
